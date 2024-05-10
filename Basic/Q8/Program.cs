a6bce3e6bc418be941152eaedce5dcd896757f93
wvrtungduong <145447160+wvrtungduong@users.noreply.github.com>
2024-05-10 18:45:45 +0700

    Add files via upload

---
+++ Basic/Q8/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the number:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Output:");
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+}
+

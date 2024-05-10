8848b2c6dce94d1422c6f59b5befdf7f259dd15c
wvrtungduong <145447160+wvrtungduong@users.noreply.github.com>
2024-05-10 19:08:05 +0700

    Add files via upload

---
+++ Basic/Q15/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Q15
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(remove_char("w3resource", 1));
+        Console.WriteLine(remove_char("w3resource", 9));
+        Console.WriteLine(remove_char("w3resource", 0));
+    }
+    public static string remove_char(string str, int n)
+    {
+        return str.Remove(n, 1);
+    }
+}

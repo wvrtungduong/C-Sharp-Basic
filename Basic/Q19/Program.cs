6f7dd13a7c169f472f754a6c8b26cceb658a3a77
wvrtungduong <145447160+wvrtungduong@users.noreply.github.com>
2024-05-10 19:14:04 +0700

    Add files via upload

---
+++ Basic/Q19/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input first integer: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second integer");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num_1 + num_2);
+if (num_1 == num_2) Console.WriteLine((num_1+num_2)*3);

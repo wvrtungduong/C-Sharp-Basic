5a7e77f21cb067ff09e3ba68dd6f458969dc40fc
wvrtungduong <145447160+wvrtungduong@users.noreply.github.com>
2024-05-13 16:17:35 +0700

    Add files via upload

---
+++ Basic/Q28/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string str = "Display the pattern like pyramid using the alphabet.";
+string[] words =  str.Split(' ');
+string result = "";
+for (int i = 1; i <= words.Length; i++)
+{
+    result = result + words[words.Length - i] + " ";
+}
+Console.WriteLine(result);

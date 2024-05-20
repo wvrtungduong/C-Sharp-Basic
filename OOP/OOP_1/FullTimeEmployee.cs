a2f1695316ad54f5c3094c14ffb38de1c148a4c2
wvrtungduong <145447160+wvrtungduong@users.noreply.github.com>
2024-05-20 22:27:45 +0700

    Add files via upload

---
+++ OOP/OOP_1/FullTimeEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    internal class FullTimeEmployee : Employee
+    {
+        public FullTimeEmployee(string name, int paymentPerHour) : base(name, paymentPerHour)
+        {
+        }
+
+        public override int calculateSalary()
+        {
+            return 8 * getPaymentPerHour();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("|{0}|Full time Employee|             |{1,16}|", getName().PadRight(32), getPaymentPerHour());
+            //return getName() + ": Full time Employee, payment per hour: " + getPaymentPerHour();
+        }
+    }
+}

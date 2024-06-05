27cd44f20d4f64d04d4f6eedc57d059fe8d508bd
wvrtungduong <wvrtungduong6102@gmail.com>
2024-06-05 16:10:14 +0700

    Update database exercise

---
+++ ManageCategoriesApp/ManageCategories.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace ManageCategoriesApp
+{
+    //Declaring record Category
+    public record Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+    //-
+
+    public class ManageCategories
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        string ConnectionString = "Server=(local);uid=sa;pwd=sa;database=MyStore;TrustServerCertificate=true";
+
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "Select CategoryID, CategoryName from Categories";
+
+            command = new SqlCommand(SQL, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                if (reader.HasRows == true)
+                {
+                    while (reader.Read())
+                    {
+                        categories.Add(new Category
+                        {
+                            CategoryID = reader.GetInt32("CategoryID"),
+                            CategoryName = reader.GetString("CategoryName")
+                        });
+                    } //end while
+                }//end if
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return categories;
+        }//end GetCategories
+         //-
+        public void InsertCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            //CategoryID is identity
+            command = new SqlCommand("Insert Categories values (@CategoryName)", connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }//end InsertCategory
+
+        public void UpdateCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "Update Categories set CategoryName=@CategoryName where CategoryID=@CategoryID";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+            command.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }// End UpdateCategory
+        public void DeleteCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "Delete Categories where CategoryID=@CategoryID";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }// End DeleteCategory
+    }//End ManageCategories
+}
